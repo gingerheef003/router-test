05b3db16a6431c1073844512351128b767988c21
Ginger Heef <gingerheef003@gmail.com>
2023-05-13 05:46:29 +0530

    Addons for the site

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,8 +3,10 @@ import { Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation } f
 import "../styles/root.css"
 import { createContact, getContacts } from "../contacts"
 
-export async function loader() {
-	const contacts = await getContacts();
+export async function loader({ request }) {
+	const url = new URL(request.url);
+	const q = url.searchParams.get("q");
+	const contacts = await getContacts(q);
 	return { contacts };
 }
 
@@ -23,7 +25,7 @@ export default function Root() {
 			<div id="sidebar">
 				<h1>React Router Contacts</h1>
 				<div>
-					<form id="search--form" role={"search"}>
+					<Form id="search--form" role={"search"}>
 						<input
 							id="q"
 							aria-label="Search contacts"
@@ -40,7 +42,7 @@ export default function Root() {
 							className="sr-only"
 							aria-live="polite"
 						></div>
-					</form>
+					</Form>
 					<Form method="post">
 						<button type="submit">New</button>
 					</Form>
